1e2038086f8613e99e5a0afbe3f96b2b33b44002
lucascruceru <lucascruceru@gmail.com>
2017-07-12 16:45:02 +0300

    first function for Door CRUD with test working

--- src/test/java/ro/fortech/DoorController.java
+++
@@ -1,4 +0,0 @@
-package ro.fortech;
-
-public class DoorController {
-}
--- src/test/java/ro/fortech/DoorControllerTests.java
+++ src/test/java/ro/fortech/DoorControllerTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +17,6 @@ import ro.fortech.entities.User;
 import ro.fortech.repositories.DoorRepository;
 import ro.fortech.repositories.UserRepository;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,13 +26,15 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
-public class ActionControllerTests {
+public class DoorControllerTests {
 
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
@@ -48,15 +48,12 @@ public class ActionControllerTests {
     private HttpMessageConverter mappingJackson2HttpMessageConverter;
 
     private List<Door> doors = new ArrayList<>();
-    private List<User> users = new ArrayList<>();
 
     private User user;
 
-    @Autowired
-    private DoorRepository doorRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    private DoorRepository doorRepository;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -78,59 +75,28 @@ public class ActionControllerTests {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
 
         this.doorRepository.deleteAllInBatch();
-        this.userRepository.deleteAllInBatch();
-
-
-        this.users.add(userRepository.save(new User(username, "password")));
 
 
         this.doors.add(doorRepository.save(new Door("GarageDoor", true)));
         this.doors.add(doorRepository.save(new Door("FrontDoor", true)));
 
 
-
     }
 
-
-
     @Test
-    public void openTest() throws Exception {
+    public void readAllDoorsTest() throws Exception {
 
-        mockMvc.perform(get("/action/" + username + "/open/"
-                + this.doors.get(0).getName()))
+        mockMvc.perform(get("/action/" + username))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("[0].closed", is(false)));
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("[0].id",is(this.doors.get(0).getId().intValue())))
+                .andExpect(jsonPath("[0].name", is("GarageDoor")))
+                .andExpect(jsonPath("[0].closed", is(true)))
+                .andExpect(jsonPath("[1].id",is(this.doors.get(1).getId().intValue())))
+                .andExpect(jsonPath("[1].name", is("FrontDoor")))
+                .andExpect(jsonPath("[1].closed", is(true)));
+
     }
 
-    @Test
-    public void closeTest() throws Exception {
-        mockMvc.perform(get("/action/" + username + "/close/"
-                + this.doors.get(0).getName()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("[0].closed", is(true)));
-    }
-    @Test
-	public void seeOptions() throws Exception {
-
-		mockMvc.perform(get("/action/" + username))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(contentType))
-				.andExpect(jsonPath("$", hasSize(2)))
-				.andExpect(jsonPath("[0].id",is(this.doors.get(0).getId().intValue())))
-				.andExpect(jsonPath("[0].name", is("GarageDoor")))
-				.andExpect(jsonPath("[0].closed", is(true)))
-				.andExpect(jsonPath("[1].id",is(this.doors.get(1).getId().intValue())))
-				.andExpect(jsonPath("[1].name", is("FrontDoor")))
-				.andExpect(jsonPath("[1].closed", is(true)));
-
-	}
-
-
-
-    private String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        this.mappingJackson2HttpMessageConverter.write(
-                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
-    }
 }
